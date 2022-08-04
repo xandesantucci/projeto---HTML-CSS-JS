153ec2ea3d2f84b063292d16646846978f7ab893
Alexandre Santucci <60152543+xandesantucci@users.noreply.github.com>
2022-08-04 17:40:26 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+
+const btnavancar = document.getElementById("btn-avancar");
+const btnvoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+function escondercartaoselecionado(){
+    const cartaselecionada = document.querySelector(".selecionado");
+    cartaselecionada.classList.remove('selecionado');
+}
+
+function mostrarcartao(indicecartao){
+    cartoes[indicecartao].classList.add('selecionado');
+}
+
+btnavancar.addEventListener("click",function(){
+
+    if(cartaoAtual === cartoes.length -1) return;
+
+    escondercartaoselecionado()
+
+    cartaoAtual++;
+    mostrarcartao(cartaoAtual);
+});
+
+
+
+
+btnvoltar.addEventListener("click",function(){
+
+    if(cartaoAtual === 0) return;
+
+    escondercartaoselecionado();
+
+    cartaoAtual--;
+    mostrarcartao(cartaoAtual);
+
+
+})
